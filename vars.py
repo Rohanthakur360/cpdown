f0d45023879ac11cc5fdae787dcda85c69b3a923
Rohanthakur360 <70991755+Rohanthakur360@users.noreply.github.com>
2024-04-14 21:26:59 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,5 +1,5 @@
-api_id = 52465674
-api_hash = "87c231f0e8705385b8239d06965b4351"
+api_id = 28919553
+api_hash = "2ed4532cc8353ab77179cb291ffeb86e"
 bot_token = "6505721336:AAFrKPJHhGf3cTq-lD2kr2nvo2qZDiJR-VA"
 auth_users = [0]
 sudo_user = 0
