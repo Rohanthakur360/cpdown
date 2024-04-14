a5235fb29acf8c9344ebfdeabaaa2b420e24a6f3
Rohanthakur360 <70991755+Rohanthakur360@users.noreply.github.com>
2024-04-14 20:34:42 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,5 +3,5 @@ api_hash = "87c231f0e8705385b8239d06965b4351"
 bot_token = "6505721336:AAFrKPJHhGf3cTq-lD2kr2nvo2qZDiJR-VA"
 auth_users = [0]
 sudo_user = 0
-log_channel = -
-txt_channel = -
+log_channel = -1002132785565
+txt_channel = -1002132785565
